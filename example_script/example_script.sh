f2073a4a0fef53da826d67e1e8350d6a745b58fb
Jeremy Magland <jeremy.magland@gmail.com>
2017-07-21 11:58:56 -0400

    update script

--- example_script/example_script.sh
+++ example_script/example_script.sh
@@ -1,2 +1,2 @@
 # Download an example dataset and run processing
-cd /work/fi_ss/analyses/manuscript/synthetic && prv-download datasets/K15/raw.mda.prv --server=river raw && kron-run ms3 K15 --_nodaemon
+cd /work/fi_ss/analyses/manuscript/synthetic && prv-download datasets/K15/raw.mda.prv --server=river raw.mda && mp-run-process mountainsort.bandpass_filter --timeseries=raw.mda --timeseries_out=filt.mda --samplerate=30000 --freq_min=300 --freq_max=6000
